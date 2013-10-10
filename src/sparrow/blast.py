f7495d922ad331787a91b960b2134f8009e082e6
djcline <djcline@gmail.com>
2013-10-10 14:53:30 +0000

    first version find reciprocal matches

---
+++ src/sparrow/blast.py
@@ -0,0 +1,40 @@
+'''
+Created on Oct 8, 2013
+
+@author: djcline
+'''
+
+import sys
+import csv
+
+
+def reciprocals(firstBlastFileName, secondBlastFileName, outFileName):
+    """Finds recriprocal best matches in two blast result files
+    with formatting 'qseqid sseqid qlen slen length pident'
+    and copies the lines that are reciprocally matched in
+    firstBlastFileName into outFileName"""
+
+    with open(firstBlastFileName) as firstBlast, open(secondBlastFileName) as secondBlast, open(outFileName, 'w') as outFile:
+        #Make dictionary from query sequence to target sequence from the first blast file
+        firstReader = csv.reader(firstBlast)
+        firstSecondMap = {row[0] : row[1] for row in firstReader}
+        
+        #now walk through the second blast file and when we find a match, print that line out
+        secondReader = csv.reader(secondBlast)
+        count = 0;
+        for row in secondReader:
+            #Reciprocal by switching columns 0 and 1 of first blast
+            if row[0] == firstSecondMap.get(row[1], None):
+                outFile.write(",".join(row) + "\n")
+                count += 1
+        
+        print count, 'reciprocal matches'
+        
+    
+    
+
+if __name__ == '__main__':
+    reciprocals(*sys.argv[1:])
+    
+    
+    
