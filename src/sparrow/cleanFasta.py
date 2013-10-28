514e3396cbc1bae4ff6cfb447ec4f8fd188c80dc
djcline <djcline@gmail.com>
2013-10-28 01:34:51 +0000

    had committed test stuff

--- src/sparrow/cleanFasta.py
+++ src/sparrow/cleanFasta.py
@@ -21,25 +21,12 @@ def listNonStandard(inFileName, outFileName):
     for seqRecord in SeqIO.parse(inFileName, "fasta"):
         count += 1;
         
-        if not count % 1000:
-            print count;
-        
-        if count == 2374232:
-            nonStandard.append(seqRecord);
-            break;
-        
         #some may be lowercase
-        #sequence=str(seqRecord.seq).upper()        
+        sequence=str(seqRecord.seq).upper()        
         
-        #if any((c not in ACCEPTABLE_CHARS) for c in sequence):
-        #    nonStandard.append(seqRecord)
-        
-        #for c in sequence:
-        #   if(c not in ACCEPTABLE_CHARS):
-        #        print seqRecord.id + ": " + c
-        
-        #if ":" in sequence:
-        #    print seqRecord.id
+        if any((c not in ACCEPTABLE_CHARS) for c in sequence):
+            nonStandard.append(seqRecord)
+
             
     SeqIO.write(nonStandard, outFileName, "fasta")
 
